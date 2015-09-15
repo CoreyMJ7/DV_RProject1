9ebdb1ec358b149e81740ae8ec0a093fac4a6c1a
Nathan Riojas <nriojas.60@gmail.com>
2015-09-14 20:05:03 -0500

    Put the graphs together, knitted html to have most of what Canata wanted

--- 01 Data/Titanic Analysis.R
+++ 01 Data/Titanic Analysis.R
@@ -1,18 +1,34 @@
 require("jsonlite")
 require("RCurl")
-# Change the USER and PASS below to be your UTEid
-df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic where sex is not null"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_UTEid', PASS='orcl_UTEid', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-df
+require("ggplot2")
+
+
+# Three data frames for specific graphing cases
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_nar784', PASS='orcl_nar784', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+df2 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic where sex is not null"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_nar784', PASS='orcl_nar784', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+df3 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic where sex is not null and age <= 10"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_nar784', PASS='orcl_nar784', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+df4 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic where sex is not null and age >= 40"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_nar784', PASS='orcl_nar784', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+
+
+#df
 summary(df)
 head(df)
 
 require(extrafont)
+
+
+
+#Graph 1: Fare vs Age w/ null included
 ggplot() + 
   coord_cartesian() + 
   scale_x_continuous() +
   scale_y_continuous() +
   #facet_wrap(~SURVIVED) +
-  facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
+  #facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
   #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
   labs(title='Titanic') +
   labs(x="Age", y=paste("Fare")) +
@@ -26,6 +42,53 @@ ggplot() +
         position=position_jitter(width=0.3, height=0)
   )
 
+
+
+#Graph 2: Fare vs Age w/o null 
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_continuous() +
+  scale_y_continuous() +
+  #facet_wrap(~SURVIVED) +
+  #facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
+  #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
+  labs(title='Titanic') +
+  labs(x="Age", y=paste("Fare")) +
+  layer(data=df2, 
+        mapping=aes(x=as.numeric(as.character(AGE)), y=as.numeric(as.character(FARE)), color=SEX), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        #position=position_identity()
+        position=position_jitter(width=0.3, height=0)
+  )
+
+
+
+#Graph 3: Fare vs Survived w/ As Character
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_discrete() +
+  scale_y_continuous() +
+  #facet_wrap(~SURVIVED) +
+  #facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
+  #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
+  labs(title='Titanic') +
+  labs(x="SURVIVED", y=paste("Fare")) +
+  layer(data=df2, 
+        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=as.character(SURVIVED)), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        #position=position_identity()
+        position=position_jitter(width=0.3, height=0)
+  )
+
+
+
+# Graph 4: Fare vs Survived w/Class Separation
 ggplot() + 
   coord_cartesian() + 
   scale_x_discrete() +
@@ -33,7 +96,7 @@ ggplot() +
   facet_grid(PCLASS~SURVIVED, labeller=label_both) +
   labs(title='Titanic') +
   labs(x="SURVIVED", y=paste("FARE")) +
-  layer(data=df, 
+  layer(data=df2, 
         mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=SEX), 
         stat="identity", 
         stat_params=list(), 
@@ -41,4 +104,50 @@ ggplot() +
         geom_params=list(), 
         #position=position_identity()
         position=position_jitter(width=0.3, height=0)
+  )
+
+
+
+
+
+#Graph 5: Fare vs Survived w/P class where age <= 10
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_discrete() +
+  scale_y_continuous() +
+  #facet_wrap(~SURVIVED) +
+  #facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
+  facet_grid(PCLASS~SURVIVED, labeller=label_both) +
+  labs(title='Titanic where age <= 10') +
+  labs(x="SURVIVED", y=paste("Fare")) +
+  layer(data=df3, 
+        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=SEX), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        #position=position_identity()
+        position=position_jitter(width=0.3, height=0)
+  )
+
+
+
+#Graph 6: Fare vs Survived w/P class where age <= 10
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_discrete() +
+  scale_y_continuous() +
+  #facet_wrap(~SURVIVED) +
+  #facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
+  facet_grid(EMBARKED~SEX, labeller=label_both) +
+  labs(title='Titanic where age >= 40') +
+  labs(x="SEX", y=paste("Fare")) +
+  layer(data=df4, 
+        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=SEX), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        #position=position_identity()
+        position=position_jitter(width=0.4, height=0)
   )
